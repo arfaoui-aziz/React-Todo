e0c5f6b234bdd774ba915552781c5d1f3f1e5233
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-03-27 13:56:18 +0100

    feat: deleting blogs

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import BlogList from "./BlogList";
+import { useState, useEffect } from "react";
+
+//******** Home Component */
 const Home = () => {
   const [blogs, setBlogs] = useState([
     { title: "My new website", body: "lorem ipsum...", author: "aziz", id: 1 },
@@ -11,14 +14,28 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("aziz");
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() => {
+    console.log("use Effect running");
+  }, [name]);
+
+  //***** Return*/
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's Blogs"
+        handleDelete={handleDelete}
+      />
+      <button onClick={() => setName("luigi")}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
