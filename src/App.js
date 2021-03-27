006dcea4f2c3505b56a02c5feb800d96c60aa62e
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2021-03-27 11:55:22 +0100

    feat: adding Blogs & style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import "./index.css";
 import Navbar from "./Navbar";
-
+import Home from "./Home";
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 };
